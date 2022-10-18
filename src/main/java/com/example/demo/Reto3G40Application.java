91d29b173981e433e399abebfcbc27a66cbeb984
Williambabativamt <william.babativa.mt@usa.edu.co>
2022-10-17 22:13:24 -0500

    v3

--- src/main/java/com/example/demo/Reto3G40Application.java
+++ src/main/java/com/example/demo/Reto3G40Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Reto3G40Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Reto3G40Application.class, args);
 	}
 
 }
